b6ff412738388f544f2a2f3b5b94a6ec7bd84ed9
ShurikPozd <shurik-3002@mail.ru>
2026-05-18 20:22:06 +0300

    Добавил функционал переводчика и сокращателя ссылок

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
-from handlers import start_help, weather, currency, anime
+from handlers import start_help, weather, currency, anime, translate, shorten
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,6 +14,8 @@ dp.include_router(start_help.router)
 dp.include_router(weather.router)
 dp.include_router(currency.router)
 dp.include_router(anime.router)
+dp.include_router(translate.router)
+dp.include_router(shorten.router)
 
 
 async def main():
--- keyboards.py
+++ keyboards.py
@@ -5,6 +5,7 @@ main_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="/weather"), KeyboardButton(text="/currency")],
         [KeyboardButton(text="/anime"), KeyboardButton(text="/cancel")],
+        [KeyboardButton(text="/translate")], [KeyboardButton(text="/shorten")],
         [KeyboardButton(text="/help")]
     ],
     resize_keyboard=True
